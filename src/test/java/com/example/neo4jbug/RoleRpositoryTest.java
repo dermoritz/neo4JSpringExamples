9970498e9984c3d0d562cf2e72f937ca9420bce2
moritz loeser <moritz.loeser@prodyna.com>
2019-02-20 17:22:55 +0100

    removed author

--- src/test/java/com/example/neo4jbug/RoleRpositoryTest.java
+++ src/test/java/com/example/neo4jbug/RoleRpositoryTest.java
@@ -12,9 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-/**
- * @author moritz.loeser@prodyna.com / external.moritz.loeser@de.bosch.com / moritz
- **/
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RoleRpositoryTest {
